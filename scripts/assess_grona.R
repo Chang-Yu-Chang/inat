1a575bc50dbea89781007f388c569a227001565b
Chang-Yu Chang <cychang@Chang-Yus-iMac.local>
2025-12-08 17:41:43 +0800

    grona figs

--- scripts/assess_grona.R
+++ scripts/assess_grona.R
@@ -1,7 +1,9 @@
 ### ============================================================
 ### 0. Libraries
 ### ============================================================
+
 library(tidyverse)
+library(twmap) # for taiwan map
 library(janitor)
 library(rinat)
 library(cowplot)
@@ -9,6 +11,7 @@ library(sf)
 library(MASS)   # kde2d
 library(FNN)    # nearest neighbor
 library(ggthemes)
+library(geosphere) # for computing distance
 
 
 ### ============================================================
@@ -41,147 +44,188 @@ gro_tw <- subset_area(gro, c(21, 26), c(120, 123)) %>%
 ### ============================================================
 ### 3. Taiwan map
 ### ============================================================
-map_tw <- map_data("world") %>%
-    as_tibble() %>%
-    filter(region == "Taiwan") %>%
-    filter(lat > 21, long > 120)
+map_tw <- st_as_sf(twmap::tw_county) %>%
+    st_crop(
+        xmin = 119.8, xmax = 123,
+        ymin = 21,  ymax = 25.4
+    )
 
-#tab_use <- read_csv("~/Downloads/98136_95-104年國土利用調查成果鄉鎮市區統計資料（95年版土地使用分類系統表，3級分類）.csv")
+# map_tw <- map_data("world") %>%
+#     as_tibble() %>%
+#     filter(region == "Taiwan") %>%
+#     filter(lat > 21, long > 120)
 
 
-
+ggplot(map_tw) +
+    geom_sf()
 ### ============================================================
 ### PANEL A — Taiwan wide-map (hexbin density + raw points)
 ### ============================================================
 pA <- ggplot() +
-    geom_polygon(data = map_tw,
-                 aes(x = long, y = lat, group = group),
-                 fill = "gray98", color = "gray50", linewidth = 0.3) +
-
+    geom_sf(data = map_tw, fill = "gray98", color = "gray50", linewidth = 0.3) +
     # density layer
-    stat_bin_hex(data = gro_tw,
-                 aes(x = longitude, y = latitude),
-                 bins = 40, alpha = 0.7) +
+    stat_bin_2d(data = gro_tw, aes(x = longitude, y = latitude), bins = 40, alpha = 0.7) +
     scale_fill_gradient(low = "grey99", high = "grey40", name = "Density") +
     # raw points
     geom_point(data = gro_tw, aes(longitude, latitude, color = scientific_name), alpha = 0.6, size = 0.3) +
+    # zoom in
+    geom_rect(aes(xmin = 121.4, xmax = 121.6, ymin = 24.9, ymax = 25.1), color = "black", fill = "NA", linewidth = .5) +
     scale_color_brewer(palette = "Set2", name = "Species") +
-    coord_fixed(clip = "off") +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 3)) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 3)) +
+    coord_sf(clip = "off") +
     theme_minimal() +
     theme(
-        legend.position = "right",
+        legend.position = "bottom",
         legend.background = element_blank()
     ) +
     guides(
-        #fill = guide_legend(direction = "horizontal")
-        color = guide_legend(override.aes = list(size = 2))
+        color = "none"
     ) +
     labs(x = "Longitude", y = "Latitude")
 
-
 ### ============================================================
-### PANEL B — Species counts barplot
+### PANEL A inset — Species counts barplot
 ### ============================================================
 species_counts <- gro_tw %>%
-    count(scientific_name)
+    count(scientific_name) %>%
+    mutate(sp_name = str_replace(scientific_name, "Grona", "G."))
 
-pB <- ggplot(species_counts) +
-    geom_col(aes(x = reorder(scientific_name, -n), y = n, fill = scientific_name), color = "black") +
+pA2 <- ggplot(species_counts) +
+    geom_col(aes(x = reorder(sp_name, -n), y = n, fill = scientific_name), color = "black") +
     scale_fill_brewer(palette = "Set2", name = NULL) +
     coord_cartesian(clip = "off") +
+    scale_y_continuous(position = "right") +
     theme_bw() +
-    labs(y = "Count") +
+    labs(x = "", y = "Count") +
     theme(
-        axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 5, face = "bold.italic"),
+        axis.text.y = element_text(size = 5),
         axis.title.x = element_blank(),
-        legend.position = "none"
+        axis.title.y = element_blank(),
+        panel.grid = element_blank(),
+        legend.position = "none",
+        plot.background = element_rect(color = "black", fill = "white", linewidth = .5)
     )
 
 
 ### ============================================================
-### PANEL C — Zoom-in panel (automatic detection of densest region)
+### Panel B - zoom in map
 ### ============================================================
-#
-# # KDE for density
-# dens <- kde2d(gro_tw$longitude, gro_tw$latitude, n = 300)
-# peak_idx <- which(dens$z == max(dens$z), arr.ind = TRUE)
-# peak_lon <- dens$x[peak_idx[1]]
-# peak_lat <- dens$y[peak_idx[2]]
-#
-# # Define a ~2 km radius bounding box
-# zoom_radius <- 0.02  # in degrees (~2 km)
-# zoom_box <- gro_tw %>%
-#     filter(between(longitude, peak_lon - zoom_radius, peak_lon + zoom_radius),
-#            between(latitude,  peak_lat - zoom_radius, peak_lat + zoom_radius))
-#
-# pC <- ggplot(zoom_box) +
-#     geom_polygon(data = map_tw,
-#                  aes(long, lat, group = group),
-#                  fill = NA, color = "gray60", linewidth = 0.2) +
-#     geom_point(aes(longitude, latitude, color = scientific_name),
-#                size = 2, alpha = 0.85) +
-#     scale_color_brewer(palette = "Set2", name = NULL) +
-#     coord_fixed(clip = "off") +
-#     theme_bw() +
-#     labs(
-# #        subtitle = sprintf("Zoomed-in region near (%.3f, %.3f)", peak_lat, peak_lon),
-#         x = "Longitude", y = "Latitude"
-#     ) +
-#     theme(legend.position = "none")
-#
+map_tw_sub <- st_crop(
+    map_tw,
+    xmin = 121.4, xmax = 121.6,
+    ymin = 24.9,  ymax = 25.1
+)
+
+gro_tw_sub <- filter(
+    gro_tw,
+    longitude > 121.4, longitude < 121.6,
+    latitude > 24.9, latitude < 25.1
+)
+
+pB <- ggplot() +
+    geom_sf(data = map_tw_sub, fill = "gray98", color = "gray50", linewidth = 0.3) +
+    # raw points
+    geom_point(data = gro_tw_sub, aes(longitude, latitude, color = scientific_name), alpha = 0.6, size = 1) +
+    scale_color_brewer(palette = "Set2", name = "Species") +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 3), expand = c(0,0)) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 3), expand = c(0,0)) +
+    coord_sf(clip = "off") +
+    theme_bw() +
+    theme(
+        legend.position = "right",
+        legend.background = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_text(size = 5),
+        axis.text.x = element_text(angle = 45, size = 5, hjust = 1),
+        panel.border = element_rect(color = "black", fill = NA, linewidth = .5)
+    ) +
+    guides(
+        color = "none"
+    ) +
+    labs(x = "Longitude", y = "Latitude")
 
 ### ============================================================
-### PANEL D — Interspecific nearest neighbor distances
+### Panel C - between species distance
 ### ============================================================
+library(tidyverse)
+library(FNN)
 
-# helper: degree to meters
+# Helper to convert degrees to meters
 deg_to_m <- function(d) d * 111000
 
-dist_list <- list()
-species_vec <- unique(gro_tw$scientific_name)
+# Function: nearest distance from species sp1 to sp2
+nearest_between_species <- function(df, sp1, sp2){
+    focal  <- df %>% filter(scientific_name == sp1)
+    others <- df %>% filter(scientific_name == sp2)
 
-for (sp in species_vec) {
-    focal  <- gro_tw %>% filter(scientific_name == sp)
-    others <- gro_tw %>% filter(scientific_name != sp)
+    if(nrow(focal) == 0 | nrow(others) == 0){
+        return(NULL)
+    }
 
-    if (nrow(focal) == 0 | nrow(others) == 0) next
-
-    focal_mat  <- as.matrix(focal[, c("longitude", "latitude")])
-    others_mat <- as.matrix(others[, c("longitude", "latitude")])
-
-    nn <- get.knnx(others_mat, focal_mat, k = 1)
-
-    focal$nn_dist_m <- deg_to_m(nn$nn.dist)
-    dist_list[[sp]] <- focal
-}
-
-distances_all <- bind_rows(dist_list)
-
-pD <- distances_all %>%
-    ggplot(aes(nn_dist_m)) +
-    geom_histogram(bins = 40,
-                   fill = "#9ecae1", color = "black", alpha = 0.85) +
-    theme_bw() +
-    coord_cartesian(clip = "off") +
-    labs(
-        #title = "D. Interspecific Nearest-Neighbor Distance",
-        #subtitle = "Distances < 50–100 m indicate frequent fine-scale co-occurrence",
-        x = "Distance to nearest other Grona species (meters)",
-        y = "Number of observations"
+    nn <- get.knnx(
+        data = as.matrix(others[, c("longitude", "latitude")]),
+        query = as.matrix(focal[, c("longitude", "latitude")]),
+        k = 1
     )
 
+    tibble(
+        sp_pair = paste(sp1, "→", sp2),
+        sp1 = sp1,
+        sp2 = sp2,
+        nn_dist_m = deg_to_m(nn$nn.dist[,1])
+    )
+}
+
+# Species present
+species <- gro_tw$scientific_name %>% unique() %>% sort()
+
+# All heterospecific pairs
+pairs <- expand_grid(sp1 = species, sp2 = species) %>%
+    filter(sp1 != sp2)
+
+# Compute distances for all 6 directional pairs
+dist_all <- map2_dfr(pairs$sp1, pairs$sp2, ~nearest_between_species(gro_tw, .x, .y))
+
+pC <- dist_all %>%
+    filter(nn_dist_m < 200) %>%
+    #filter(sp1 %in% c("Grona triflora") | sp1 %in% c("Grona heterophylla")) %>%
+    ggplot(aes(nn_dist_m, fill = sp_pair)) +
+    geom_histogram(alpha = 0.3, bins = 40, position = "identity") +
+    scale_fill_brewer(palette = "Set1") +
+    scale_y_continuous(breaks = scales::pretty_breaks(n=5)) +
+    facet_grid(sp1~.) +
+    coord_cartesian(clip = "off") +
+    labs(
+        x = "Nearest heterospecific distance (meters)",
+        y = "Count",
+        fill = "Species Pair"
+    ) +
+    theme_bw() +
+    theme(
+        legend.position = "none",
+        panel.grid.minor = element_blank(),
+        strip.background = element_blank()
+    )
 
 ### ============================================================
 ### Combine all panels
 ### ============================================================
-p_full <- plot_grid(
-    pA,
-    plot_grid(pB, pD, ncol = 1, labels = c("B", "C"), align = "v", axis = "lr"),
-    ncol = 2, labels = c("A", ""),
-    rel_widths = c(1.2, 1), scale = c(0.95, 1),
-    align = "hv", axis = "tl"
-) +
-    theme(plot.background = element_rect(color = NA, fill = "white"))
+pB2 <- ggdraw() + draw_image(here::here("plots/grona.jpg"), scale = 1)
 
-ggsave("plots/grona-four-panel.png", p_full, width = 10, height = 6, dpi = 300)
+p <- plot_grid(
+    pA,
+    plot_grid(
+        plot_grid(pB, pB2, nrow = 1, rel_widths = c(1, 1.2), labels = c("B", "C"), label_x = c(-0.1, 0)),
+        pC,
+        rel_heights = c(1,2),
+        ncol = 1, scale = .95, labels = c("", "D")),
+    ncol = 2, labels = c("A", ""),
+    rel_widths = c(1, 1.2), scale = c(1, 1)
+) +
+    theme(plot.background = element_rect(color = NA, fill = "white")) +
+    draw_plot(pA2, .03, .75, .15, .23)
+
+ggsave("plots/grona.png", p, width = 8, height = 6, dpi = 300)
 
